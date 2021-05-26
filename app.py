1faf95313cedc16efd9e22ba6d0e5e50e40a7570
Michael Dijk <michaeldijk@outlook.com>
2021-05-26 14:46:55 +0100

    connected MongoDB, connected route to template

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import os
-from flask import Flask
+from flask import (Flask, flash, render_template, 
+                    redirect, request, session, url_for)
+from flask.templating import render_template
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+
+
 # if local env. import env.py, otherwise not
 if os.path.exists("env.py"):
     import env
@@ -8,12 +15,23 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello Michael"
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
-# 
+# Setup instance of PyMongo linking to Flask App
+mongo = PyMongo(app)
+
+
+@app.route("/") # default route (index/homepage)
+@app.route("/get_stories") # stories route (goes to homepage)
+def get_stories():
+    stories = mongo.db.stories.find() # find stories db, and assign it to variable stories
+    return render_template("stories.html", stories=stories) # use variable stories, and assign it to stories
+
+
+# run app, with environment variables from env.py local, otherwise from settings in Heroku
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), 
             port=int(os.environ.get("PORT")),
