1c30efd4a7059ba2de779ac012519c65a32a0cc8
Michael Dijk <michaeldijk@outlook.com>
2021-05-27 10:52:15 +0100

    added login route/function

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import (Flask, flash, render_template, 
                     redirect, request, session, url_for)
 import flask
@@ -62,7 +63,28 @@ def register():
 # Login template route
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # check if username is present in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            # ensure the password matches from db and user's input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")): 
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+        else:
+            # username does not exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
     return render_template("login.html")
+
 
 # run app, with environment variables from env.py local, otherwise from settings in Heroku
 if __name__ == "__main__":
