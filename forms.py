c408808564477c6477e52cdd6d1d8f629a55164c
Michael Dijk <michaeldijk@outlook.com>
2021-05-28 13:11:18 +0100

    added comments to where I found help for code

--- forms.py
+++ forms.py
@@ -1,13 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-# Import DataRequired for required field, length for max length, EqualTo for equal to password field, Regexp for checking for input, and NoneOf for removing certain user accounts
+# Import DataRequired for required field, length for max length, 
+# EqualTo for equal to password field, Regexp for checking for input,
+# and NoneOf for removing certain user accounts
 from wtforms.validators import DataRequired, Length, EqualTo, Regexp, NoneOf
 
 
+# Found help for using WTForms, from: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iii-web-forms
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(),
                             Length(min=5, max=15, message="Username, Minimum 5 maximum 15 characters."),
                             Regexp("^[a-zA-Z0-9]{5,15}$", flags=0, message="Username can only contain small letters, and numbers"),
+                            # Found help for NoneOf on https://stackoverflow.com/questions/58464698/anyof-validation-in-flask
                             NoneOf(["admin", "root", "superuser", "adminaccount", "Administrator", "administrator"], message="Username is not permitted", values_formatter=None)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=5, max=15, message="Password, Minimum 5 maximum 15 characters."),
                             Regexp("^[a-zA-Z0-9]{5,15}$", flags=0, message="Password can only contain small letters, and numbers")])
@@ -19,4 +23,4 @@ class RegisterForm(FlaskForm):
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Register')
+    submit = SubmitField('Login')
