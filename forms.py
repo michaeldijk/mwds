5d7fab63c98494801d385c80a0f6e41d1d67d57f
Michael Dijk <michaeldijk@outlook.com>
2021-05-28 12:44:37 +0100

    added defensive checks to register form

--- forms.py
+++ forms.py
@@ -1,13 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo, Regexp
+# Import DataRequired for required field, length for max length, EqualTo for equal to password field, Regexp for checking for input, and NoneOf for removing certain user accounts
+from wtforms.validators import DataRequired, Length, EqualTo, Regexp, NoneOf
 
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(),
-                            Length(min=5, max=15, message="Minimum 5 maximum 15 characters."),
+                            Length(min=5, max=15, message="Username, Minimum 5 maximum 15 characters."),
+                            Regexp("^[a-zA-Z0-9]{5,15}$", flags=0, message="Username can only contain small letters, and numbers"),
+                            NoneOf(["admin", "root", "superuser", "adminaccount", "Administrator", "administrator"], message="Username is not permitted", values_formatter=None)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=5, max=15, message="Password, Minimum 5 maximum 15 characters."),
                             Regexp("^[a-zA-Z0-9]{5,15}$", flags=0, message="Password can only contain small letters, and numbers")])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=5, max=15, message="Minimum 5 maximum 15 characters.")])
     password_confirm = PasswordField(
                        'Confirm Password', validators=[DataRequired(), 
                         EqualTo('password', message="Passwords do not match."), Length(min=5, max=15, message="Minimum 5 maximum 15 characters.")])
